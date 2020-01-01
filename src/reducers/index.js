d797c4cb1b5fe28b4a9f32aaf734ef3ca692a3d0
tyapama <tyapama@gmail.com>
2020-01-01 21:12:42 +0900

    implement event creation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,13 +13,13 @@
 
 const events = (state=[], action) => {
   switch(action.type){
-    case CREATE:
+    case 'CREATE_EVENT':
       const event = {title: action.title, body: action.body}
       const length = state.length
       let id = length === 0 ? 1 : state[length - 1 ].id +1
       return [...state,{id, ...event}]
-    case DELETE:
-    case ALL_DELETE:
+    case 'DELETE':
+    case 'ALL_DELETE':
     default:
        throw new Error
   }
